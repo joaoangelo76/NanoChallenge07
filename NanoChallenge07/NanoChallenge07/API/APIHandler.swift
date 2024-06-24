27ef628b5b9e0eb96ac7a322b82f67fa08607124
kauamiguel <kauamcm@gmail.com>
2024-06-24 16:00:35 -0300

    sucks

---
+++ NanoChallenge07/NanoChallenge07/API/APIHandler.swift
@@ -0,0 +1,8 @@
+//
+//  APIHandler.swift
+//  NanoChallenge07
+//
+//  Created by Kaua Miguel on 24/06/24.
+//
+
+import Foundation
---
+++ NanoChallenge07/NanoChallenge07/DataBase/CoreDataHandler.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataHandler.swift
+//  NanoChallenge07
+//
+//  Created by Kaua Miguel on 24/06/24.
+//
+
+import Foundation
