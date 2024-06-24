b55fffbd423949334ff6f856e0759ea5db3cab16
João Ângelo <134436082+joaoangelo76@users.noreply.github.com>
2024-06-24 15:31:00 -0300

    Adding base of the project

---
+++ NanoChallenge07/NanoChallenge07/NanoChallenge07App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge07App.swift
+//  NanoChallenge07
+//
+//  Created by João Ângelo on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge07App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
