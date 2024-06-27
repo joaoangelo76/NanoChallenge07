ea04c54cf130da4a41d9683ba154a195b043d5b5
kauamiguel <kauamcm@gmail.com>
2024-06-27 15:46:13 -0300

    Test search bar correctly

--- Nano07Challenge/Nano07ChallengeUITests/Nano07ChallengeUITests.swift
+++ Nano07Challenge/Nano07ChallengeUITests/Nano07ChallengeUITests.swift
@@ -17,8 +17,22 @@ final class Nano07ChallengeUITests: XCTestCase {
 
     }
 
+
     func testSearchBar() throws {
-       
+        
+        XCUIApplication().launch()
+        let app = XCUIApplication()
+        app.textFields["Start typing to search..."].tap()
+        
+        let aKey = app/*@START_MENU_TOKEN@*/.keys["A"]/*[[".keyboards.keys[\"A\"]",".keys[\"A\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        aKey.tap()
+        
+        let fKey = app/*@START_MENU_TOKEN@*/.keys["f"]/*[[".keyboards.keys[\"f\"]",".keys[\"f\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        fKey.tap()
+        
+        let aKey2 = app/*@START_MENU_TOKEN@*/.keys["a"]/*[[".keyboards.keys[\"a\"]",".keys[\"a\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        aKey2.tap()
+        app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"retorno\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
     }
 
     func testLaunchPerformance() throws {
